eb14d7a54c72744e7901d5e9e7091c82d4c4f4e0
Savoury SnaX <savoury.snax@googlemail.com>
2021-01-07 14:14:58 +0000

    remove old remote debugger code

--- src/host/logfile.c
+++ src/host/logfile.c
@@ -14,52 +14,18 @@
 
 FILE* logFile=NULL;
 
-#if ENABLE_REMOTE_DEBUG
-#include <pthread.h>
-
-extern int useRemoteDebugger;
-
-char remoteDebuggerLog[1024*1024]={0};
-
-extern pthread_mutex_t commandSyncMutex;
-#endif
-
 void CONSOLE_OUTPUT(char* fmt,...)
 {
 	va_list args;
 
-#if ENABLE_REMOTE_DEBUG
-	if (useRemoteDebugger)
+	if (logFile == NULL)
 	{
-		char tmpForLog[32768];
-		
-		va_start(args,fmt);
-
-		vsprintf(tmpForLog,fmt,args);
-		
-		va_end(args);
-
-		pthread_mutex_lock(&commandSyncMutex);
-
-		if (strlen(tmpForLog)<(sizeof(remoteDebuggerLog)-strlen(remoteDebuggerLog)))
-		{
-			strcat(remoteDebuggerLog,tmpForLog);
-		}
-
-		pthread_mutex_unlock(&commandSyncMutex);
+		logFile = fopen(LOGFILENAME, "w");
 	}
-	else
-#endif
-	{
-		if (logFile==NULL)
-		{
-			logFile=fopen(LOGFILENAME,"w");
-		}
 
-		va_start(args,fmt);
-		vprintf(fmt,args);
-		vfprintf(logFile,fmt,args);
-		va_end(args);
-	}
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	vfprintf(logFile, fmt, args);
+	va_end(args);
 }
 
